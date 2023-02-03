6a8019ab9fa0718f2f4bf776900202264f636e19
AmiR Bin <ur.amirbin@gmail.com>
2023-02-03 16:39:27 +0330

    Add box behaviour

---
+++ Assets/Level 2/Boxo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boxo : MonoBehaviour {
+    [SerializeField] private Rigidbody2D boxRb;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.transform.CompareTag("Bullet")) {
+            DropBox();
+        }
+    }
+
+    private void DropBox() {
+        boxRb.isKinematic = false;
+    }
+}
