ba5cfe2f6666b2eef5b2e1f82bedf24e59582732
Alizadev <mohammadalizadeh1995@gmail.com>
2023-02-03 14:59:38 +0330

    character sprite, scene, scripts, animations

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,9 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public bool controlable = true;
+    public bool isPlayerHidden;
+
+    public int health;
     public float speed;
-    public float sitSpeed;
+    //public float sitSpeed;
     public float jumpForce;
 
     public float dashSpeed;
@@ -12,11 +18,13 @@ public class PlayerControl : MonoBehaviour
     private float horizontal;
     private float dashTime;
     private bool isDashing;
-    private bool isSitting;
+    //private bool isSitting;
     private float dashDirection;
     private bool isFacingRight = false;
     private Rigidbody2D playerRig;
+    private Animator playerAnim;
 
+    [SerializeField] private SpriteRenderer playerRenderer;
     public Transform groundCheck;
     public LayerMask groundLayer;
 
@@ -24,16 +32,17 @@ public class PlayerControl : MonoBehaviour
     private float AngleDeg;
     public Transform weaponAim;
 
+    float tention;
+
     public Weapon currentWeapon;
 
     public GameManager _GameManager;
-    [SerializeField] private float closetRange = 1f;
-    [SerializeField] private SpriteRenderer renderer;
-    private bool isPlayerHidden;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        playerAnim = GetComponent<Animator>();
         playerRig = GetComponent<Rigidbody2D>();
     }
 
@@ -41,11 +50,36 @@ public class PlayerControl : MonoBehaviour
 
     private void Update()
     {
+        //animation
+        playerAnim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        if (horizontal > 0)
+        {
+            playerAnim.SetFloat("walkSpeed", isFacingRight ? -1 : 1);
+        }
+        else
+        {
+            playerAnim.SetFloat("walkSpeed", isFacingRight ? 1 : -1);
+        }
+        playerAnim.SetBool("isJumping", !isGrounded());
+
+
+        //control
+        if (controlable && isPlayerHidden == false)
+        {
+            
+        }
+        else
+        {
+            horizontal = 0;
+            return;
+        }
+
         horizontal = Input.GetAxis("Horizontal");
-        Hidden();
+
         //jump
         if (!isDashing && Input.GetButtonDown("Jump") && isGrounded())
         {
+            playerAnim.SetBool("isJumping", true);
             playerRig.velocity += new Vector2(playerRig.velocity.x, jumpForce);
         }
 
@@ -56,12 +90,12 @@ public class PlayerControl : MonoBehaviour
         Flip();
 
         //sitting
-        if (!isDashing && Input.GetKeyDown(KeyCode.S) && isGrounded())
-        {
-            isSitting = !isSitting;
-        }
+        //if (Input.GetKeyDown(KeyCode.S) && isGrounded())
+        //{
+        //    isSitting = !isSitting;
+        //}
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing && !isSitting)
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)// && !isSitting)
         {
             isDashing = true;
             dashTime = dashDuration;
@@ -84,6 +118,7 @@ public class PlayerControl : MonoBehaviour
 
         _GameManager.debugText.text = "Angle Deg: " + AngleDeg.ToString("0");
         _GameManager.debugText.text += "\nfacing right: " + isFacingRight;
+        _GameManager.debugText.text += "\ngrounded: " + isGrounded();
         //pick
         if (Input.GetKeyDown(KeyCode.E))
         {
@@ -118,21 +153,51 @@ public class PlayerControl : MonoBehaviour
         //shoot
         if (currentWeapon != null)
         {
-            if (Input.GetKeyDown(KeyCode.Mouse0))
+            if (currentWeapon.model == "Bow")
             {
-                Shoot();
+                if (Input.GetKey(KeyCode.Mouse0))
+                {
+                    tention = Mathf.MoveTowards(tention, 1f, 2f * Time.deltaTime);
+                }
+                if (Input.GetKeyUp(KeyCode.Mouse0))
+                {
+                    Shoot();
+                }
+                
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    Shoot();
+                }
             }
         }
 
     }
     void FixedUpdate()
     {
-        if (isPlayerHidden)
+        if (controlable)
         {
-            isDashing = false;
-            playerRig.velocity = Vector2.zero;
+            playerRig.isKinematic = false;
+        }
+        else
+        {
+            playerRig.isKinematic = true;
             return;
         }
+
+        if (isPlayerHidden)
+        {
+            playerRig.velocity = Vector2.zero;
+            //hop out
+            //if (!isDashing && Input.GetButtonDown("Jump") && isGrounded())
+            //{
+            //    isPlayerHidden = false;
+            //}
+            return;
+        }
+
         //Dash
         if (isDashing)
         {
@@ -148,57 +213,33 @@ public class PlayerControl : MonoBehaviour
                 playerRig.velocity = new Vector2(dashDirection * dashSpeed, playerRig.velocity.y);
             }
         }
-        //Move
-        else if (!isSitting)
-        {
-            playerRig.velocity = new Vector2(horizontal * speed, playerRig.velocity.y);
-        }
-        else
-        {
-            playerRig.velocity = new Vector2(horizontal * sitSpeed, playerRig.velocity.y);
-        }
+        //sit
+        //else if (!isSitting)
+        //{
+        playerRig.velocity = new Vector2(horizontal * speed, playerRig.velocity.y);
+        //}
+        //else
+        //{
+        //    playerRig.velocity = new Vector2(horizontal * sitSpeed, playerRig.velocity.y);
+        //}
 
-    }
-
-    private void Hidden()
-    {
-        if (!isDashing && Input.GetKeyDown(KeyCode.H) && isGrounded())
-        {
-            Collider2D closet = Physics2D.OverlapCircle(transform.position, closetRange, LayerMask.GetMask("Closet"));
-            if (closet != null)
-            {
-                if (isPlayerHidden)
-                {
-                    gameObject.layer = LayerMask.NameToLayer("Player");
-                    renderer.color = Color.white;
-                }
-                else
-                {
-                    transform.position = new Vector2(closet.transform.position.x, transform.position.y);
-                    gameObject.layer = LayerMask.NameToLayer("Hidden");
-                    renderer.color = Color.gray;
-                }
-
-                isPlayerHidden = !isPlayerHidden;
-            }
-        }
     }
 
     private bool isGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
     }
 
     void Flip()
     {
 
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1;
-            transform.localScale = localScale;
-        }
+        //if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        //{
+        //    isFacingRight = !isFacingRight;
+        //    Vector3 localScale = transform.localScale;
+        //    localScale.x *= -1;
+        //    transform.localScale = localScale;
+        //}
 
         if (currentWeapon)
         {
@@ -206,29 +247,70 @@ public class PlayerControl : MonoBehaviour
         }
 
         //aim based
-        //isFacingRight = Mathf.Abs(AngleDeg) > 90;
+        isFacingRight = Mathf.Abs(AngleDeg) > 90;
 
-        //Vector3 localScale = transform.localScale;
-        //localScale.x = isFacingRight ? -1 : 1;
-        //transform.localScale = localScale;
+        Vector3 localScale = transform.localScale;
+        localScale.x = isFacingRight ? -1 : 1;
+        transform.localScale = localScale;
     }
 
     void Shoot()
     {
         if (currentWeapon.ammo > 0)
         {
-            GameObject bullet = Instantiate(currentWeapon.bulletType, weaponAim.transform.position + weaponAim.transform.right, weaponAim.transform.rotation * Quaternion.Euler(0, 0, 90));
+            GameObject bullet = Instantiate(currentWeapon.bulletType, weaponAim.transform.position + (weaponAim.transform.right * 1f), weaponAim.transform.rotation * Quaternion.Euler(0, 0, 90));
             Bullet bulletScript = bullet.GetComponent<Bullet>();
-            bulletScript.rg.AddForce(weaponAim.right * 10, ForceMode2D.Impulse);
+
+            if (currentWeapon.model == "Bow")
+            {
+                bulletScript.rg.AddForce(weaponAim.right * bulletScript.speed * Mathf.Clamp(tention, 0.25f, 1f), ForceMode2D.Impulse);
+                bulletScript.tention = tention;
+                tention = 0;
+            }
+            else
+            {
+                bulletScript.rg.AddForce(weaponAim.right * bulletScript.speed, ForceMode2D.Impulse);
+            }
             currentWeapon.ammo--;
         }
     }
 
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.transform.name.Contains("Weapon"))
-    //    {
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Closet"))
+        {
+            if (!isDashing && Input.GetButtonDown("Jump"))// && isGrounded())
+            {
+                if (isPlayerHidden)
+                {
+                    //gameObject.layer = LayerMask.NameToLayer("Player");
+                    playerRenderer.color = Color.white;
+                }
+                else
+                {
+                    transform.position = new Vector2(collision.transform.position.x, transform.position.y);
+                    //gameObject.layer = LayerMask.NameToLayer("Hidden");
+                    playerRenderer.color = Color.gray;
+                }
 
-    //    }
-    //}
+                isPlayerHidden = !isPlayerHidden;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //collect ammo
+        if (currentWeapon)
+        {
+            if (currentWeapon.model == "Bow")
+            {
+                if (collision.transform.name.Contains("Arrow"))
+                {
+                    Destroy(collision.gameObject);
+                    currentWeapon.ammo++;
+                }
+            }
+        }
+    }
 }
